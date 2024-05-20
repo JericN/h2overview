8cdde4b208ec95fbf63eff7978d6999c393eecaa
draqunov10 <ledrhoniel@gmail.com>
2024-05-20 22:04:14 +0800

    draft of h2overview basic

--- h2overview.ino
+++ h2overview.ino
@@ -1,53 +1,100 @@
-#define pressureSensorPin A0
-int flowSensorPin = 2;
 
-int relay = 3;
+#define pressureSensorPin     A0
+#define pressureOffset        0.483
+
+#define flowSensorPin         D2
+#define flowCalibrationFactor 4.5 // TODO: Calibrate this value
+
+#define solenoidRelayPin      D3
+#define SOLENOID_OPEN         0x1
+#define SOLENOID_CLOSED       0x0
+
+// Global variables for the flow sensor
 volatile long pulse;
-float volume;
-int openSolenoid = 0;
+float volume = 0;
 float lastTime = 0.0;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(flowSensorPin, INPUT);
-  Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(flowSensorPin), increase, RISING);
 
-  pinMode(relay, OUTPUT);
+// =============================================================================
+// ============================== PHYSICAL LAYER ===============================
+// =============================================================================
 
+// Read the pressure sensor and convert it to psi
+float read_pressure() {
+  float pressureVoltage = analogRead(pressureSensorPin) * 5.0 / 1023.0;
+  float pressure = (pressureVoltage - pressureOffset) * (100.0 / (4.5 - baselinePressureVoltage));
+  return pressure;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Read the water volume in L
+float read_water_volume() { return volume; }
 
-  // Water flow sensor
-  volume = 2.663 * pulse;
+// Read the water flow rate in L/s
+float read_waterflow_rate() {
   if (millis() - lastTime > 1000) {
     pulse = 0;
     lastTime = millis();
   }
+  return 2.663 * pulse;;
+}
+
+// Read the solenoid state. 0x1 is open and 0x0 is closed
+uint_8t get_solenoid_state() { return digitalRead(solenoidRelayPin); }
+
+// Set the solenoid state. 0x1 is open and 0x0 is closed
+void set_solenoid_state(uint_8t STATE) { digitalWrite(solenoidRelayPin, STATE); }
+
+
+// =============================================================================
+// ============================= DEVICE FUNCTIONS ==============================
+// =============================================================================
+
+// Once the solenoid button is pressed, the solenoid state will be toggled
+void switch_solendoid_state() {
+  if (get_solenoid_state() == SOLENOID_OPEN) { set_solenoid_state(SOLENOID_CLOSED); }
+  else { set_solenoid_state(SOLENOID_OPEN); }
+}
+
+// Update the volume of the water every loop
+void update_volume() { volume = volume + read_waterflow_rate(); }
+
+// Update pulse in Hz
+void pulse_counter() { pulse++; }
+
+
+// =============================================================================
+// ============================== SETUP AND LOOP ===============================
+// =============================================================================
+
+void setup() {
+  // Initialize the serial communication
+  Serial.begin(9600);
+
+  // Initialize the pins
+  pinMode(flowSensorPin, INPUT);
+  pinMode(solenoidRelayPin, OUTPUT);
+
+  // FIXME: Start the solenoid with solenoid state in the database
+  digitalWrite(solenoidRelayPin, LOW);
+
+  // Attach the interrupt to the flow sensor
+  attachInterrupt(digitalPinToInterrupt(flowSensorPin), pulse_counter, RISING);
+
+}
+
+void loop() {
+  Serial.print("Volume: ");
+  Serial.print(read_water_volume());
+  Serial.println(" mL");
+
   Serial.print("Flow Rate: ");
-  Serial.print(volume);
+  Serial.print(read_waterflow_rate());
   Serial.println(" mL/s");
-1
-  // solenoid
-  // if (openSolenoid == 1) {
-  //   digitalWrite(relay, HIGH);
-  // } else {
-  //   digitalWrite(relay, LOW);
-  // }
 
-  // Pressure sensor
-  int pressureSensorValue = analogRead(pressureSensorPin);
-  float pressureVoltage = pressureSensorValue * (5.0 / 1023.0);
-  float baselinePressureVoltage = 0.45;
-  float pressure = (pressureVoltage - baselinePressureVoltage) * (100.0 / (4.5 - baselinePressureVoltage));
   Serial.print("Pressure: ");
-  Serial.print(pressure);
+  Serial.print(read_pressure());
   Serial.println(" psi");
-  delay(1000);
+
+  delay(5000);
 }
 
-void increase() {
-  pulse++;
-}
