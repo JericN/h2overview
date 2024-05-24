dfbe9192a9d7635615c896bba4b597457a5184f0
Jeric <jmnarte@up.edu.ph>
2024-05-25 01:41:31 +0800

    feat: add migrating files to mqtt

---
+++ h2overview-mqtt/server.cpp
@@ -0,0 +1,80 @@
+#include "server.h"
+
+// Initialize static variable
+Firebase FirebaseServer::firebase(REFERENCE_URL);
+String FirebaseServer::pref_path = "devices/" + String(DEVICE_SERIAL) + "/preference";
+String FirebaseServer::flag_path = "devices/" + String(DEVICE_SERIAL) + "/flags";
+String FirebaseServer::data_path = "devices/" + String(DEVICE_SERIAL) + "/data";
+
+FirebaseServer::FirebaseServer() {
+  // Constructor
+}
+
+// =================================================================
+// =========================== PREFENCES ===========================
+// =================================================================
+
+LeakPref FirebaseServer::get_small_leak_preference() {
+  // Fetch the data from Firebase
+  String data = firebase.getString(pref_path + "/leak_scan");
+
+  // Deserialize the data
+  JsonDocument doc;
+  deserializeJson(doc, data);
+
+  // Extract the deserialized data
+  bool scheduled = doc["scheduled"];
+  unsigned long duration = doc["duration"];
+  unsigned long start_time = doc["start_time"];
+  unsigned long end_time = doc["end_time"];
+
+  return LeakPref{scheduled, duration, start_time, end_time};
+}
+
+// =================================================================
+// ============================= FLAGS =============================
+// =================================================================
+
+bool FirebaseServer::get_valve_state() {
+  return firebase.getInt("devices/H2O-1234/flags/valve_state");
+}
+
+bool FirebaseServer::is_leak_scanning() {
+  return firebase.getInt(flag_path + "/leak_scanning");
+}
+
+bool FirebaseServer::is_leak_detected() {
+  return firebase.getInt(flag_path + "/leak_detected");
+}
+
+void FirebaseServer::set_valve_state(int state) {
+  int res = firebase.setInt(flag_path + "/valve_state", state);
+  if (res == FAILURE) {
+    Serial.println("[ERROR] Firebase: Failed to set valve_state flag");
+  }
+}
+
+void FirebaseServer::set_leak_scanning(int state) {
+  int res = firebase.setInt(flag_path + "/leak_scanning", state);
+  if (res == FAILURE) {
+    Serial.println("[ERROR] Firebase: Failed to set leak_scanning flag");
+  }
+}
+
+void FirebaseServer::set_leak_detected(int state) {
+  int res = firebase.setInt(flag_path + "/leak_detected", state);
+  if (res == FAILURE) {
+    Serial.println("[ERROR] Firebase: Failed to set leak_detected flag");
+  }
+}
+
+// ==================================================================
+// ============================== DATA ==============================
+// ==================================================================
+
+void FirebaseServer::send_waterflow(Waterflow data) {
+  int res = firebase.setFloat(data_path + "/water_flow/" + String(data.timestamp), data.value);
+  if (res == FAILURE) {
+    Serial.println("Failed to send Waterflow data to Firebase");
+  }
+}
